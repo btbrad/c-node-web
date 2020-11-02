d681c6c4081f067105f3a099ea65fd7d2c451918
btbrad <biantaobrad@gmail.com>
2020-11-02 23:45:30 +0800

    添加reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import list from './reducer'
 
-const store = createStore(combineReducers({}), applyMiddleware(thunk))
+const store = createStore(combineReducers({
+  list
+}), applyMiddleware(thunk))
 
 export default store
